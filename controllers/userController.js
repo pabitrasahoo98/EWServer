ac5c719f00c19c76ff866a1b0c8fca0a9c911a39
Pabitra Sahoo <pabitrachin2@gmail.com>
2024-12-30 00:12:04 +0530

    first commit

--- controllers/userController.js
+++ controllers/userController.js
@@ -109,7 +109,7 @@ exports.loginUser = catchAsyncError(async (req, res, next) => {
 });
 //logout user
 exports.logout=catchAsyncError(async(req,res,next)=>{
-    res.cookie("token",null,{expires:new Date(Date.now()),httpOnly:true,});
+    res.cookie("token",null,{expires:new Date(Date.now())});
     res.status(200).json({
         sucess:true,
         message:"logged out"
